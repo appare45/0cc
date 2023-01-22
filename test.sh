8c235e0e892c3648c8737be3455438cf9e3f7aa3
appare45 <64473501+appare45@users.noreply.github.com>
2023-01-23 01:34:34 +0900

    added multi charcter operator (==, !=, >=, <=)

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+all=0
+fail=0
+
 assert() {
   expected="$1"
   input="$2"
@@ -8,11 +11,14 @@ assert() {
   ./tmp
   actual="$?"
 
+  ((++all))
+
   if [ "$actual" = "$expected" ]; then
-    echo "$input => $actual"
+    echo "✅ Success: $input => $actual"
   else
-    echo "$input => $expected expected, but got $actual"
-    exit 1
+    echo "🚫 Fail: $input => $expected expected, but got $actual"
+    ((++fail))
+    # exit 1
   fi
 }
 
@@ -27,4 +33,21 @@ assert 10 "-10+20"
 assert 10 '- -10'
 assert 10 '- - +10'
 
-echo OK
+assert 1 "0==0"
+assert 0 "0==1"
+assert 0 "0!=0"
+assert 1 "10!=0"
+assert 1 "10>0"
+assert 0 "10>20"
+assert 0 "10>10"
+assert 0 "10<0"
+assert 1 "10<20"
+assert 0 "10<10"
+assert 1 "10>=10"
+assert 1 "20>=10"
+assert 0 "10>=20"
+assert 1 "10<=10"
+assert 0 "20<=10"
+assert 1 "10<=20"
+
+echo "Failed: $fail / $all"
